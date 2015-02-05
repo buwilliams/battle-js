f6cee14ee6eab833048b99a88029c52a3ec28c53
Buddy Williams <buddywilliams@gmail.com>
2015-02-04 20:33:30 -0500

    renamed props to attrs to be consistent;

--- app/features/engine/mover.js
+++ app/features/engine/mover.js
@@ -14,7 +14,7 @@ function() {
     var out = function(x, y, mass) {
 
         // Initial values
-        var props = {
+        var attrs = {
             _mass: 1,
             _location:     { x: 0, y: 0 },
             _velocity:     { x: 0, y: 0 }, // x = xspeed and y = yspeed
@@ -23,54 +23,54 @@ function() {
         };
 
         // You can pass in a default starting position and mass
-        props._location.x = (typeof x === 'undefined') ? props._location.x : x;
-        props._location.y = (typeof y === 'undefined') ? props._location.y : y;
-        props._mass = (typeof mass === 'undefined') ? props._mass : mass;
+        attrs._location.x = (typeof x === 'undefined') ? attrs._location.x : x;
+        attrs._location.y = (typeof y === 'undefined') ? attrs._location.y : y;
+        attrs._mass = (typeof mass === 'undefined') ? attrs._mass : mass;
 
         // Main public function to update location
-        props.update = function() {
+        attrs.update = function() {
 
             // These are the things that make motion possible such as
             // wind, gravity, jumping, friction, etc.
-            props.applyForces();
+            attrs.applyForces();
 
             // Update veloicty by acceleration
             // Acceleration is interesting because since we are
             // running this in a game loop we will be getting
             // faster or slower depending on the forces which are
             // applied
-            props._velocity.x += props._acceleration.x;
-            props._velocity.y += props._acceleration.y;
+            attrs._velocity.x += attrs._acceleration.x;
+            attrs._velocity.y += attrs._acceleration.y;
 
             // Update target location with velocity
-            props._location.x += props._velocity.x;
-            props._location.y += props._velocity.y;
+            attrs._location.x += attrs._velocity.x;
+            attrs._location.y += attrs._velocity.y;
         }
 
         // Functions for applying forces
         // forces are interesting because they accumulate
-        props.applyForces = function() {
-            for(var i=0; i<props._forces.length; i++) {
-                var force = props._forces[i];
-                props._acceleration.x += force.x;
-                props._acceleration.y += force.y;
+        attrs.applyForces = function() {
+            for(var i=0; i<attrs._forces.length; i++) {
+                var force = attrs._forces[i];
+                attrs._acceleration.x += force.x;
+                attrs._acceleration.y += force.y;
             }
         };
 
         // Force: {x: 0, y: 0, name: 'wind'}
-        props.addForce = function(force) {
-            props._forces.push(force);
+        attrs.addForce = function(force) {
+            attrs._forces.push(force);
         };
 
-        props.removeForce = function(name) {
-            for(var i=0; i<props._forces.length; i++) {
-                if(props._forces[i].name === name) {
-                    props._forces.splice(i, 1);
+        attrs.removeForce = function(name) {
+            for(var i=0; i<attrs._forces.length; i++) {
+                if(attrs._forces[i].name === name) {
+                    attrs._forces.splice(i, 1);
                 }
             }
         };
 
-        return props;
+        return attrs;
     };
 
     return out;
